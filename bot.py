b3fb5fcd3cbc3a9ecbc9849fd24d7dfcac5967c1
boyplunger128 <tranbao1414@gmail.com>
2022-12-14 14:49:21 +0700

    save not bug file

--- bot.py
+++ bot.py
@@ -33,15 +33,15 @@ def getListCoins():
                 busdData[i]=busdData[j];
                 busdData[j]=temp; 
     listName = [];
-    for i in range(100):
+    for i in range(int(os.getenv('AMOUNT_TOP_COIN'))):
         if('BUSD' in busdData[i]['symbol']):
             listName.append(busdData[i]['symbol']);
     #print(f.read());
     return listName;
 
-def getHistoryCandle(SYMBOL):    
+def getHistoryCandle(SYMBOL,interval):    
     params = SYMBOL.strip("''");
-    url = 'https://api.binance.com/api/v3/klines?symbol='+params+'&interval=1h&limit=2';
+    url = 'https://api.binance.com/api/v3/klines?symbol='+params+'&interval='+interval+'&limit=2';
     request =   requests.get(url);
     time.sleep(5);
     listData = request.json();
@@ -55,7 +55,7 @@ def getMaValue(SYMBOL,PERIOD):
     #print(LastSymol);
     SecretKey = os.getenv('API_KEY_INDIC');
 
-    url = 'https://api.taapi.io/ma?secret='+SecretKey+'&exchange=binance&symbol='+LastSymol+'&interval=1h&period='+PERIOD;
+    url = 'https://api.taapi.io/ma?secret='+SecretKey+'&exchange=binance&symbol='+LastSymol+'&interval='+os.getenv('INTERVAL2')+'&period='+PERIOD;
     request =   requests.get(url);
     time.sleep(3);
     data = request.json();
@@ -79,11 +79,11 @@ def getMultiIndiValue(SYMBOL,interval):
             "indicators": [             
             {
                 "indicator":"ma",
-                "period":20
+                "period":os.getenv('PERIOD20')
             },
             {        
                 "indicator": "ma",
-                "period": 100 
+                "period": os.getenv('PERIOD100')
             }
             ]
         }
@@ -123,7 +123,7 @@ dispatcher = updater.dispatcher;
 def startCommand(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to crypto hunter");
     context.bot.send_message(chat_id=update.effective_chat.id, text="Please wait... Bot is checking... ^^");
-    
+   
     listCoins = getListCoins();
 
     #print(listCoins);
@@ -131,20 +131,30 @@ def startCommand(update: Update, context: CallbackContext):
         try:
             #result_obj = eval(result); -- convert string to object, but is no need now
             image = get(result['url']).content;
+            symbol = result['name'].replace('BUSD','');
+            link_buy ='\n'+ result['message']+'\nYou can buy it here: https://www.binance.com/vi/trade/'+symbol+'_BUSD?theme=dark&type=spot \n';
             time.sleep(5);
             if image:
-                context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(image, caption="")])
-                context.bot.send_message(chat_id=update.effective_chat.id, text=result['message']);
-                context.bot.send_message(chat_id=update.effective_chat.id, text=result['name']); 
+                context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto(image, caption=link_buy)]);
+                #context.bot.send_message(chat_id=update.effective_chat.id, text=result['message']);
+                #context.bot.send_message(chat_id=update.effective_chat.id, text=link_buy,disable_web_page_preview=True); 
         except Exception:
             traceback.print_exc();
         time.sleep(5);
+    
+    def sayhello():
+        context.bot.send_message(chat_id=update.effective_chat.id, text="This is thread 2",disable_web_page_preview=True); 
+
     i=0;
-    def task():
-        if(i%1440==0):
+
+    interval = '15m';
+
+    def task(interval):
+        if(i%24==0):
             t_updatelistCoins();
 
         for coin in listCoins:
+            print(coin,len(listCoins));
             try:
                 result ={
                     "url":"",
@@ -152,7 +162,8 @@ def startCommand(update: Update, context: CallbackContext):
                     "message":""
                 }
                 #print(coin);
-                coinHis = getHistoryCandle(coin.strip("''"));
+                
+                coinHis = getHistoryCandle(coin.strip("''"),interval);
                 #print(coinHis);
                 time.sleep(2);
                 openPrice = coinHis[0][1];
@@ -160,23 +171,23 @@ def startCommand(update: Update, context: CallbackContext):
                 messageBox ='';
                 flag20 = 0;
                 flag100 = 0;
-                maValues = getMultiIndiValue(coin,'1h');
+                maValues = getMultiIndiValue(coin,interval);
                 time.sleep(2);
                 if(float(openPrice) < maValues[0] and maValues[0] < float(closePrice)):
                     flag20=1;
                 if(float(openPrice) < maValues[1] and maValues[1] < float(closePrice)):
-                    flag20=1;
+                    flag100=1;
                 if(flag100!=0):
                     if(flag20!=0):
-                        messageBox = coin +' has passed MA100 & MA20';
+                        messageBox ='\n'+ coin +' PASSED MA100 & MA20';
                     else:
-                        messageBox = coin +' has passed MA100';
+                        messageBox ='\n'+coin +' PASSED MA100';
                 else:
-                        if(flag20!=0):
-                            messageBox = coin+' has passed Ma20';
-                        alertCoin = coin+' checked!';
-                if(flag20!=0 or flag100!=0):
-                    Url = 'https://api.chart-img.com/v1/tradingview/advanced-chart?height=500&studies=MA:20&studies=MA:100&studies=RSI&symbol='+coin.strip("''")+'&key='+os.getenv('YOUR_API_KEY_CHART');
+                    if(flag20!=0):
+                        messageBox = coin+' PASSED MA20';
+                if(flag20!=0):
+                    #fix interval here
+                    Url = 'h;ttps://api.chart-img.com/v1/tradingview/advanced-chart?interval='+interval+'&symbol='+coin+'&studies=MA:20&studies=RSI&key='+os.getenv('YOUR_API_KEY_CHART');
                     #print(Url);
                     result['url']=Url;
                     result['message']=messageBox;
@@ -186,13 +197,17 @@ def startCommand(update: Update, context: CallbackContext):
         
                     time.sleep(5);
                     execbot(result);
-                else:
-                    context.bot.send_message(chat_id=update.effective_chat.id, text=alertCoin);
+                # else:
+                #     context.bot.send_message(chat_id=update.effective_chat.id, text=alertCoin);
             except Exception:
                 traceback.print_exc();  
                 time.sleep(1); 
     i=i+1;
-    context.job_queue.run_repeating(task(),1800,context=update.effective_chat.id);    
+    context.job_queue.run_repeating(task(),1200,context=update.effective_chat.id);
+    print('bot stopped'); 
+
+  
+
    
     
 
@@ -202,5 +217,5 @@ def messageHandler(update: Update, context: CallbackContext):
 dispatcher.add_handler(CommandHandler("start", startCommand))
 dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
 
-updater.start_polling();
+updater.start_polling(timeout=100);
 
